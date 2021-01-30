71a610b58554f9b7c9e5e86e356f7cd7b0c5a6eb
chu <chuusatetsuya@chuusatetsuyanoMacBook-Pro.local>
2021-01-31 05:58:13 +0900

    新規投稿機能new.createストロングパラメータ実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,12 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
   end
+
+  def creat
+    Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:messages).permit(:title, :content)
 end
